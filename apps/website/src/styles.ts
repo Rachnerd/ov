14106c869f262a471854c9a311e0609b05c70b36
Rachnerd <rachel@openvalue.nl>
2026-04-25 19:10:14 +0200

    Split up css files in prod builds. Serve fonts.

---
+++ apps/website/src/styles.ts
@@ -0,0 +1,8 @@
+/**
+ * Each import becomes a separate chunk, which ensures the styles are loaded in parallel.
+ */
+import('@ov/style/base/reset.css');
+import('@ov/style/tokens/semantic.css');
+import('@ov/style/tokens/motion.css');
+import('@ov/style/tokens/primitives.css');
+import('@ov/style/base/fonts.css');
